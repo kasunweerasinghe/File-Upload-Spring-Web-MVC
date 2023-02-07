405480d30f6567d06e7336a9a7f176232747dda5
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-07 10:14:18 +0530

    content update

--- FileUploadBackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ FileUploadBackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -7,5 +7,12 @@
 
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.spring")
 public class WebAppConfig {
 }
