7a70529e2abc132c5a26fb114db7b5fee2f11808
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-07 10:19:12 +0530

    content update

--- FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/FileUploadController.java
+++ FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/FileUploadController.java
@@ -7,5 +7,12 @@
 
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("upload")
+@CrossOrigin
 public class FileUploadController {
 }
